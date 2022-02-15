ebc99e751c942fccee589cf964c379e4ba1a5d8e
PiAir <PiAir@users.noreply.github.com>
2022-02-15 20:15:38 +0100

    Update config.php

--- bbuddy-mqtt/config.php
+++ bbuddy-mqtt/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-define('MQTT_BROKER_HOST', '192.168.0.201');
+define('MQTT_BROKER_HOST', '127.0.0.1');
 define('MQTT_BROKER_PORT', 1883);
 define('MQTT_BROKER_TLS_PORT', 8883);
 
@@ -11,5 +11,5 @@ define('TLS_CLIENT_CERTIFICATE_FILE', '');
 define('TLS_CLIENT_CERTIFICATE_KEY_FILE', '');
 define('TLS_CLIENT_CERTIFICATE_KEY_PASSPHRASE', null);
 
-define('AUTHORIZATION_USERNAME', 'admin');
-define('AUTHORIZATION_PASSWORD', 'admin');
+define('AUTHORIZATION_USERNAME', '');
+define('AUTHORIZATION_PASSWORD', '');
